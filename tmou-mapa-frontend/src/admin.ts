32b31b8e79d2e6ae7c543eca1cfdf495db8feca0
Mila Votradovec <mila@snyk.io>
2020-09-09 21:00:15 +0200

    add different colors to teams by level

--- tmou-mapa-frontend/src/admin.ts
+++ tmou-mapa-frontend/src/admin.ts
@@ -2,15 +2,16 @@ import {getMap} from './map';
 import {
   Circle,
 } from "leaflet";
-import {getTeamsPositions} from './nodes';
+import {getTeamsPositions} from './api';
 
 const mapInstance = getMap('map', [49.195, 16.609], 15);
+const colors = ['#000000aa', '#ffd83c', '#28d428', '#2972ff', '#ff2929', '#9346ba']
 
 async function run() {
   const teamsPositions = await getTeamsPositions();
 
   for (const team of teamsPositions) {
-    const c = new Circle(team.position.latLng, 18, {color: "salmon", fillOpacity: 1, interactive: true});
+    const c = new Circle(team.position.latLng, 18, {color: colors[team.level], fillOpacity: 1, interactive: true});
     c.bindTooltip(team.teamName);
     c.addTo(mapInstance);
   }
