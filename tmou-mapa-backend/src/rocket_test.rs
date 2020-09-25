c1f13963ef5bc7521c73e40f17031e5be1f8dcd6
Mila Votradovec <mila@snyk.io>
2020-09-25 22:21:51 +0200

    fix: guard index route with cookies

---
+++ tmou-mapa-backend/src/rocket_test.rs
@@ -0,0 +1,24 @@
+use super::rocket;
+use rocket::local::Client;
+use rocket::http::Status;
+
+#[test]
+fn team_without_cookie_and_phrase_is_redirected() {
+    let client = Client::new(rocket()).expect("valid rocket instance");
+    let response = client.get("/").dispatch();
+    assert_eq!(response.status(), Status::TemporaryRedirect);
+}
+
+#[test]
+fn team_without_phrase_is_redirected() {
+    let client = Client::new(rocket()).expect("valid rocket instance");
+    let response = client.get("/non-existing-phrase").dispatch();
+    assert_eq!(response.status(), Status::TemporaryRedirect);
+}
+
+#[test]
+fn admin_is_protected_by_password() {
+    let client = Client::new(rocket()).expect("valid rocket instance");
+    let response = client.get("/admin").dispatch();
+    assert_eq!(response.status(), Status::Unauthorized);
+}
