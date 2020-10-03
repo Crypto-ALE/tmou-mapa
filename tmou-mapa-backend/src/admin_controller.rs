394eb7d54392a1db10fa4a9a9f6dc2d8fbfec228
Mila Votradovec <mila@snyk.io>
2020-10-03 16:45:49 +0200

    feat: team stats

--- tmou-mapa-backend/src/admin_controller.rs
+++ tmou-mapa-backend/src/admin_controller.rs
@@ -1,6 +1,7 @@
 use super::api_models as api;
 use super::db_models as db;
 use super::db_controller::{DbControl};
+use std::collections::HashMap;
 use super::errors::*;
 use itertools::*;
 
@@ -29,11 +30,45 @@ pub fn get_teams_standings(db_control: & impl DbControl) -> TmouResult<api::Stan
     let badge_labels_db = db_control.get_badge_labels()?;
     let teams_badges_db = db_control.get_teams_badges()?;
 
-    dbg!(badge_labels_db, teams_badges_db);
-    todo!();
-    //let badge_labels = badge_labels_db.iter().map_into().collect();
-    //let standings = teams_badges_db.iter().map_into().collect();
-    //Ok(api::Standings{badge_labels, standings})
+    let potential_badges_per_team = teams_badges_db.iter().map(|tb| (&tb.team_name, match (&tb.badge_name, tb.timestamp) {
+        (Some(bn), Some(t)) => Some(Badge {
+            badge_name: bn.to_string(),
+            badge_timestamp: t,
+        }),
+        _ => None
+    }
+    )).into_group_map();
+
+    // Potentially suboptimal, could be concatenated to stuff above, but much more straight forward
+    // and readable
+    let mut badges_per_team: HashMap<&str, BadgesResults> = HashMap::new();
+    for (k,v) in potential_badges_per_team {
+        let badges: Vec<Badge> = v.into_iter().filter_map(std::convert::identity).collect();
+        let last_badge = badges.iter().max_by_key(|b| b.badge_timestamp).and_then(|b| Some(b.badge_timestamp));
+        badges_per_team.insert(k, BadgesResults {
+            badges,
+            last_badge,
+        });
+    }
+
+    let sorted_badges_per_team = badges_per_team.iter()
+        .sorted_by(|(_,v), (_,w)| {
+            match v.badges.len().cmp(&w.badges.len())
+            {
+                std::cmp::Ordering::Equal => v.last_badge.cmp(&w.last_badge),
+                o => o.reverse()
+            }
+        });
+
+    let standings: Vec<api::TeamStanding> = sorted_badges_per_team.enumerate().map(|(i, (k,v))| {
+        api::TeamStanding {
+            rank: (i+1) as u16,
+            name: k.to_string(),
+            badge_timestamps: v.badges.iter().map(|b: &Badge| {(b.badge_name.to_string(), b.badge_timestamp)}).collect()
+        }
+    }).collect();
+
+    Ok(api::Standings{badge_labels: badge_labels_db, standings})
 }
 
 ////////////////////////////////////////////////////////////////////
@@ -54,4 +89,16 @@ impl From<&db::TeamPosition> for api::TeamPosition
             level: value.level.unwrap_or(0),
         }
     }
+}
+
+#[derive(Debug)]
+struct Badge {
+    badge_name: String,
+    badge_timestamp: chrono::NaiveDateTime,
+}
+
+#[derive(Debug)]
+struct BadgesResults {
+    badges: Vec<Badge>,
+    last_badge: Option<chrono::NaiveDateTime>
 }
