e0c33ad9d3ee514197d6af91e162f5672f60e5e0
Mila Votradovec <me@milavotradovec.cz>
2021-09-12 15:45:05 +0200

    fix: airways

--- tmou-mapa-frontend/src/config.ts
+++ tmou-mapa-frontend/src/config.ts
@@ -2,8 +2,12 @@ export const config = {
 	tagColors: {
 		Europe: "#0085C7",
 		Africa: "#000000",
+		'Europe,Africa': "#000000",
+		'Africa,Asia': "#F4C300",
 		Asia: "#F4C300",
+		'Asia,Australia': "#009F3D",
 		Australia: "#009F3D",
+		'Australia,America': "#DF0024",
 		America: "#DF0024",
 		Sifra11: "#800080",
 	}
